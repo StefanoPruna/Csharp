77391412bbab248fab54615c5ac4ab3785954847
StefanoPruna <stefano.pruna@googlemail.com>
2021-06-08 11:58:24 +1000

    Finished the code

--- largest.cs
+++ largest.cs
@@ -1,15 +1,26 @@
-public class Program
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Rextester
 {
-    public static void Main()
+    public class Program
     {
-        int[] myArr = {2,7,3,8,10,5};
-        int largest = myArr[0];
-        for (int i=0; i<myArr.Length; i++)
+        public static void Main(string[] args)
         {
-            if(myArr[i] > largest)
-                largest = myArr[i];
+            //Create the integer array and a var
+            int[] myArr = {2,7,3,8,10,5};
+            int largest = myArr[0];
+            //loop through the array
+            for (int i=0;i<myArr.Length;i++)
+            {
+                //when finds the biggest number, it assigns it to the var
+                if(myArr[i] > largest)
+                    largest = myArr[i];
+            }
+            
+            Console.WriteLine(largest);
         }
-        
-        Console.Writeline(largest);
     }
 }
